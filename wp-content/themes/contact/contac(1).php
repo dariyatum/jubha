117df29a68aaf90c06729e02b41c2cf77da1fb16
Rith0068 <sothearithkoem@gmail.com>
2026-01-09 13:00:06 +0700

    change all code in php

--- wp-content/themes/contact/contac(1).php
+++ wp-content/themes/contact/contac(1).php
@@ -2,12 +2,183 @@
 
 
 
-<!--back ground contact-->
+<section>
+    <!--back ground contact-->
     <div class="backgroun_contact_page">
         <div class="text_background_contact-page">
-            <div class="Get_in_Touch_background_contact-page">Get in Touch</div>
+            Get in Touch
+            <div class="Get_in_Touch_background_contact-page">
+                <p>
+                    <i class="fa-solid fa-house"></i>
+                    /Contact/Get in Touch
+                </p>
+            </div>
         </div>
     </div>
+</section><br>
+<section>
+<!--tittle write about get in touch-->
+    <div class="tittle__text__getintouch">
+        <h1>
+            Get in touch
+        </h1>
+    </div>
+    <div class="p__text__ingetintouch">
+        <p>
+            We serve our clients from multiple locations in Saudi Arabia. In case of inquiries, complaints or suggestions please 
+            <br>contact us through one of the beneath listed channels.
+        </p>
+    </div>
+<!--photo and info get in touch-->
+    <div class="box__same__futer__but__ingetintouch">
+
+
+        <div class="same__futer__but__ingetintouch">      
+            <div class="left_same__futer__but__ingetintouch">
+                <div>
+                    <h4>
+                        For inquiries or appointments, 
+                    </h4>
+                </div>
+                <div>
+                    <h4>
+                        call our care center at:
+                    </h4>
+                </div>
+                <div>
+                    <h4>
+                    <i class="fa-solid fa-phone"></i>
+                        920033440
+                    </h4>
+                </div>
+                <div>
+                    <h4>
+                        _Or_
+                     </h4>
+                </div>
+                <div class="box__buttom__getintouch">
+                    <button onclick="" class="buttom_getintouch">
+                        <h4>
+                            book an appointments
+                        </h4>
+                    </button>
+                </div>
+            </div>
+            <img src="http://jubha_hospital.test/wp-content/uploads/2026/01/Screenshot-from-2026-01-09-08-33-12.png" alt="" class="photo__getintouch">  
+        </div>
+        <div class="line__ingetintouch"><hr></div>
+
+
+        <div class="box__email_telephone_inhetintouch">
+
+            <div class="p__email_telephone_ingetintouch">
+                <h6>
+                    Headquarter's Address
+                    <p>
+                    P.O. Box: 3568, Al Khobar 31952,
+                    <br>Kingdom of Saudi Arabia
+                    <br><a href="">
+                    Map
+                    </a>
+                </p>
+                </h6>
+            </div>
+
+            <div class="email_telephone_inhetintouch">
+                <div>
+                    <h4>
+                        <span>
+                        <i class="fa-solid fa-phone"></i>
+                        </span><br>
+                        Telephone
+                        <br>
+                        <p>
+                            920033440
+                        </p>
+                    </h4>
+                </div>
+                <div>
+                    
+                    <h4>
+                        <span>
+                        <i class="fa-solid fa-inbox"></i>   
+                        </span><br>
+                        Email
+                        <br>
+                        <p>
+                            info@almanahospital.com
+                        </p>
+                    </h4>
+                </div>
+                <div>
+                    
+                    <h4>
+                        <span>
+                        <i class="fa-solid fa-microphone"></i>  
+                        </span><br>
+                       Social Media
+                       <p>
+                            info@almanahospital.com
+                        </p>
+                    </h4>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</section>
+<!--contact form-->
+<section>
+    <div class="getintouch__contact__form">
+        <h1>
+            Contact form
+        </h1>
+    </div>
+    <div class="getintouch__contact__form__p">
+        <p>
+            If you want to find out more about the Almana Hospital, or to know more about our services, doctors,departments, 
+            <br>you can fill the following form or send your inquiries to info@almanahospital.com.
+        </p>
+    </div>
+    <div class="form__cotact__getintouch">
+        <form action="">
+            <div>
+                <label for="">First name</label>
+                <br>
+                <input type="text">
+            </div>
+            <div>
+                <label for="">Phone number</label>
+                <br>
+                <input type="text">
+            </div>
+            <div>
+                <label for="">Email</label>
+                <br>
+                <input type="email">
+            </div>
+            <div>
+                <label for="message_type">Message Type</label><br>
+                <select name="message_type" id="message_type" required>
+                    <option value="">-- Select Type --</option>
+                    <option value="inquiries_complaints">Inquiries & Complaints</option>
+                    <option value="suggestions">Suggestions</option>
+                </select>
+            </div>
+            <div>
+                <label for="">Massage</label>
+                <br>
+                <input type="text" class="message__input">
+            </div>
+        </form>
+    </div>
+</section>
+
+    
+
+
+
 
 
 

7175f00b8b80837b0ce76c0391b848c60fb6c941
Rith0068 <sothearithkoem@gmail.com>
2026-01-09 11:32:52 +0700

    contact is done

---
+++ wp-content/themes/contact/contac(1).php
@@ -0,0 +1,19 @@
+<?php get_header() ?>
+
+
+
+<!--back ground contact-->
+    <div class="backgroun_contact_page">
+        <div class="text_background_contact-page">
+            <div class="Get_in_Touch_background_contact-page">Get in Touch</div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+<?php get_footer() ?>

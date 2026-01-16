d1bf8c4f39990f5051ab2e453ff6cfc0dbba9cc8
Rith0068 <sothearithkoem@gmail.com>
2026-01-16 08:26:48 +0700

    feat video

---
+++ wp-content/themes/compaint/page-video.php
@@ -0,0 +1,131 @@
+<?php get_header();?>
+<link rel="stylesheet" href="wp-content/themes/compaint/stylesheet/video.css">
+<link rel="stylesheet" href="wp-content/themes/juha/stylesheet/video.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+<br>
+        <!--   back ground vedio-->
+    <div class="backgroun_page_video">
+        <div class="text_background_page_video">
+            News 
+            <div class="page_video__text">
+                <p>
+                <i class="fa-solid fa-house"></i>
+                Media/video libraly
+                </p>
+            </div>
+        </div>
+    </div>
+
+  
+</section><br>
+
+<section>
+    <div class="box__selech__year">
+        <div class="selech__year">
+            <form action="">
+                <input type="seach" placeholder="type the keyword">
+                <select name="year" id="">
+                    <option value="2026">2026 Info</option>
+                    <option value="2025">2025 Info</option>
+                    <option value="2024">2024 Info</option>
+                    <option value="2023">2023 Info</option>
+                    <option value="2022">2022 Info</option>
+                    <option value="2021">2021 Info</option>
+                    <option value="2020">2020 Info</option>
+                    <option value="2019">2019 Info</option>
+                    <option value="2018">2018 Info</option>
+                    <option value="2017">2017 Info</option>
+                </select>
+                <button onclick="">Search</button>  
+            </form>
+        </div>
+    </div>
+<!--video-->
+<div class="box_and_box">
+    <div class="box_video">
+
+        <div class="video">
+            <div class="video-text">
+                <h3>
+                    03/sep/20024 | 
+                    <i class="fa-solid fa-camera"></i>
+                    video
+                </h3>
+            </div>
+            <div class="icon">
+                <a href="https://youtu.be/3cJmKNeM7Ss?si=Zml9cdi80uISi_zR">
+                    <span>
+                        <i class="fa-solid fa-play"></i>
+                    </span>
+                </a>
+            </div>
+        </div>
+
+    </div>
+    <div class="box_video">
+
+        <div class="video">
+            <div class="video-text">
+                <h3>
+                    03/sep/20024 | 
+                    <i class="fa-solid fa-camera"></i>
+                    video
+                </h3>
+            </div>
+            <div class="icon">
+                <a href="https://youtu.be/3cJmKNeM7Ss?si=Zml9cdi80uISi_zR">
+                    <span>
+                        <i class="fa-solid fa-play"></i>
+                    </span>
+                </a>
+            </div>
+        </div>
+
+    </div>
+</div>
+<br><br>
+<div class="box_and_box">
+
+    <div class="box_video">
+
+        <div class="video">
+            <div class="video-text">
+                <h3>
+                    03/sep/20024 | 
+                    <i class="fa-solid fa-camera"></i>
+                    video
+                </h3>
+            </div>
+            <<div class="icon">
+                <a href="https://youtu.be/3cJmKNeM7Ss?si=Zml9cdi80uISi_zR">
+                    <span>
+                        <i class="fa-solid fa-play"></i>
+                    </span>
+                </a>
+            </div>
+        </div>
+
+    </div>
+        <div class="box_video">
+
+        <div class="video">
+            <div class="video-text">
+                <h3>
+                    03/sep/20024 | 
+                    <i class="fa-solid fa-camera"></i>
+                    video
+                </h3>
+            </div>
+            <div class="icon">
+                <a href="https://youtu.be/3cJmKNeM7Ss?si=Zml9cdi80uISi_zR">
+                    <span>
+                        <i class="fa-solid fa-play"></i>
+                    </span>
+                </a>
+            </div>
+        </div>
+
+    </div>
+</div>
+<br>
+<?php get_footer();?>
